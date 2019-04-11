eb7a1b493f3168e9b5cf67500359713b53804d83
Thiyagarajan Manihatty Bojan <thiyagarajan.mb@gmail.com>
2019-04-11 15:53:12 +0300

    Nomicam_plot included

--- Spline_interpolation.R
+++ Spline_interpolation.R
@@ -43,10 +43,17 @@ Final<-data.frame("EV" = Final_EV, "LUX" = Final_Lux)
 # Write CSV in R
 write.csv(Final, file = "C:\\Users\\varjo\\Desktop\\Final_interpolation.csv")
 
-
 #*******************************************************************************************************
 
 
+Nomi <- read.csv("C:\\Users\\varjo\\Desktop\\NOMI_Data_O.csv", header = TRUE)
+names(Nomi)<-c("Luminance_1","Y_1", "Luminance_2","Y_2", "Luminance_3","Y_3")
+attach(Nomi)
+#par(mfrow = c(3,1))
+plot(Luminance_1,Y_1, xlab = "Luminance (cd/m2)",ylab = "Y (pixel)", type="l", lty=2, lwd=3 )
+lines(Luminance_2,Y_2, xlab = "Luminance (cd/m2)",ylab = "Y (pixel)", type="l", lty=3, lwd=3 )
+lines(Luminance_3,Y_3, xlab = "Luminance (cd/m2)",ylab = "Y (pixel)", type="l", lty=4, lwd=3 )
 
-
-
+title("Mapping Luminance to Luma Pixel intensity ")
+# Add a legend
+legend(295, 95, legend=c("Data1", "Data2", "Data3"), lty=2:4, lwd=3, cex=0.8)
